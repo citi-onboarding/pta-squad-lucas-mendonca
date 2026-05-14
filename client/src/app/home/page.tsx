180eae4ab9f843acbf99e917cffa1d98f911a926
Pedro Ribeiro <phquixada@gmail.com>
2026-05-14 09:21:30 -0300

    feat/header-libraryDataCard: Add card from shadcn to /components/ui. Add LibraryDataCard logic  to /components. Add LibraryDataCard to home page.

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,12 +1,45 @@
 import Header from "@/components/Header";
+import LibraryDataCard from "@/components/LibraryDataCard";
+import { BookOpen, Clock, AlertCircle } from "lucide-react";
+
+
 
 export default function TestPage() {
+  
+  const livrosAtrasados = 12;
+
   return (
     <div>
       <Header />
-      <main style={{ padding: "2rem" }}>
+        <main className="w-full p-8">
+
         <h1 className="text-2xl font-bold text-gray-800">Dashboard</h1>
-        <p className="text-gray-500">Bem-vindo ao dashboard!</p>
+        <p className="text-gray-500">Visão geral da biblioteca</p>
+
+        <div className="mt-6 grid w-full grid-cols-3 gap-4">
+          
+          <LibraryDataCard
+            title="Total livros"
+            data="1,245"
+            icon={<BookOpen size={22}/>}
+            variant="default"
+          />
+
+          <LibraryDataCard
+            title="Empreśtimos Ativos"
+            data="87"
+            icon={<Clock size={22}/>}
+            variant="default"
+          />
+            
+          <LibraryDataCard
+            title="Livros Atrasados"
+            data={livrosAtrasados}
+            icon={<AlertCircle size={22}/>}
+            variant={livrosAtrasados > 0 ? "alert" : "default"}          
+          />
+
+        </div>
       </main>
     </div>
   );
