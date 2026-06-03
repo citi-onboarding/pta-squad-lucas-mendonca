29c90f743ee0df12fdd2e70cf666921eec82fbbc
Gabriel Nobrega <gabrielnobrega.070@gmail.com>
2026-06-03 01:37:12 -0300

    feat: create feature ui animations for buttons and page transition

--- client/src/components/registerBookForm/index.tsx
+++ client/src/components/registerBookForm/index.tsx
@@ -89,28 +89,28 @@ const onSubmit = (data: BookFormData) => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col gap-1">
             <label className="text-sm font-medium text-gray-700">Título</label>
-            <input {...register("title")} className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition" />
+            <input {...register("title")} placeholder="Digite o título do livro..." className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition" />
             {errors.title && <p className="text-red-500 text-sm mt-1">*Este é um campo obrigatório.</p>}
           </div>
 
 
           <div className="flex flex-col gap-1">
             <label className="text-sm font-medium text-gray-700">Autor</label>
-            <input {...register("author")} className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition" />
+            <input {...register("author")} placeholder="Digite o autor do livro..." className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition" />
             {errors.author && <p className="text-red-500 text-sm mt-1">*Este é um campo obrigatório.</p>}
           </div>
 
 
           <div className="flex flex-col gap-1">
             <label className="text-sm font-medium text-gray-700">ISBN</label>
-            <input {...register("isbn")} className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition" />
+            <input {...register("isbn")} placeholder="Digite o ISBN do livro..." className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition" />
             {errors.isbn && <p className="text-red-500 text-sm mt-1">*Este é um campo obrigatório.</p>}
           </div>
 
 
           <div className="flex flex-col gap-1">
             <label className="text-sm font-medium text-gray-700">Editora</label>
-            <input {...register("publisher")} className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition" />
+            <input {...register("publisher")} placeholder="Digite a editora do livro..." className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition" />
             {errors.publisher && <p className="text-red-500 text-sm mt-1">*Este é um campo obrigatório.</p>}
           </div>
 
@@ -119,6 +119,7 @@ const onSubmit = (data: BookFormData) => {
             <label className="text-sm font-medium text-gray-700">Ano</label>
             <input
                 type="number"
+                placeholder="Digite o ano de lançamento do livro..."
                 {...register("year")}
                 className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"/>
                 {errors.year && (<p className="text-red-500 text-sm mt-1">*Este é um campo obrigatório.</p>)}
@@ -127,7 +128,7 @@ const onSubmit = (data: BookFormData) => {
 
           <div className="flex flex-col gap-1">
             <label className="text-sm font-medium text-gray-700">Quantidade</label>
-            <input type="number" {...register("quantity")} className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" />
+            <input type="number" {...register("quantity")} placeholder="Digite a quantidade de livros..." className="border p-2.5 bg-gray-50 rounded-lg outline-none focus:border-primary-blue transition [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" />
             {errors.quantity && <p className="text-red-500 text-sm mt-1">*Este é um campo obrigatório.</p>}
           </div>
         </div>
